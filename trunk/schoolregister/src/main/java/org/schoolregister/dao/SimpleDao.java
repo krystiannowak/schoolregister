dd9c03860d9abaa84614f597e0849a33a5010264
Krystian.Nowak <Krystian.Nowak@3d4250a1-ed31-0410-8926-6bc40706e74d>
2007-06-02 22:16:10 +0000

    updated

---
+++ trunk/schoolregister/src/main/java/org/schoolregister/dao/SimpleDao.java
@@ -0,0 +1,5 @@
+package org.schoolregister.dao;
+
+public class SimpleDao {
+
+}
---
+++ trunk/schoolregister/src/test/java/org/schoolregister/dao/SimpleDaoTest.java
@@ -0,0 +1,13 @@
+package org.schoolregister.dao;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+
+import junit.framework.TestCase;
+
+public class SimpleDaoTest extends TestCase {
+	public void test() {
+		AnnotationConfiguration cfg = new AnnotationConfiguration();
+
+	}
+
+}
