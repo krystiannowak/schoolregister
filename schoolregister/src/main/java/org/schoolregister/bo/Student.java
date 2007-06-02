f28794942b08124393f213ce455a595ed4d6ce1e
Krystian.Nowak <Krystian.Nowak@3d4250a1-ed31-0410-8926-6bc40706e74d>
2007-06-02 22:16:10 +0000

    updated

---
+++ schoolregister/src/main/java/org/schoolregister/bo/Student.java
@@ -0,0 +1,11 @@
+package org.schoolregister.bo;
+
+public interface Student {
+
+	public Long getId();
+
+	public String getFirstName();
+
+	public String getLastName();
+
+}
---
+++ schoolregister/src/main/java/org/schoolregister/web/ApplicationServlet.java
@@ -0,0 +1,5 @@
+package org.schoolregister.web;
+
+public class ApplicationServlet {
+
+}
