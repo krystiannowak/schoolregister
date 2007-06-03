80bfc36f2ea714115cfca1349c00797fbc7db05d
Krystian.Nowak <Krystian.Nowak@3d4250a1-ed31-0410-8926-6bc40706e74d>
2007-06-03 20:14:24 +0000

    JPA almost ready

--- trunk/schoolregister/src/test/java/org/schoolregister/dao/SimpleDaoTest.java
+++ trunk/schoolregister/src/test/java/org/schoolregister/dao/SimpleDaoTest.java
@@ -1,16 +1,24 @@
 package org.schoolregister.dao;
 
+import javax.persistence.*;
+
 import junit.framework.*;
 
 import org.hibernate.*;
 import org.hibernate.cfg.*;
 
 public class SimpleDaoTest extends TestCase {
-	public void test() {
+	public void testHibernateConfig() {
 		AnnotationConfiguration cfg = new AnnotationConfiguration();
 		cfg.configure("hibernate.cfg.xml");
 		SessionFactory sessionFactory = cfg.buildSessionFactory();
+	}
 
+	public void testPersistenceConfig() {
+		System.out.println("a");
+		//EntityManagerFactory emf = Persistence
+		//		.createEntityManagerFactory("schoolregister");
+		//emf.close();
 	}
 
 }
