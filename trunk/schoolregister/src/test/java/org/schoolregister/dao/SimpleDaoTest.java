97c60d0d2a668ebc8c2f36080acde1cb56931557
Krystian.Nowak <Krystian.Nowak@3d4250a1-ed31-0410-8926-6bc40706e74d>
2007-06-03 18:16:20 +0000

    small configuration refactoring

--- trunk/schoolregister/src/test/java/org/schoolregister/dao/SimpleDaoTest.java
+++ trunk/schoolregister/src/test/java/org/schoolregister/dao/SimpleDaoTest.java
@@ -4,13 +4,11 @@ import junit.framework.*;
 
 import org.hibernate.*;
 import org.hibernate.cfg.*;
-import org.schoolregister.bo.*;
 
 public class SimpleDaoTest extends TestCase {
 	public void test() {
 		AnnotationConfiguration cfg = new AnnotationConfiguration();
-		cfg.configure("hibernate-cfg.xml");
-		cfg.addAnnotatedClass(StudentImpl.class);
+		cfg.configure("hibernate.cfg.xml");
 		SessionFactory sessionFactory = cfg.buildSessionFactory();
 
 	}
