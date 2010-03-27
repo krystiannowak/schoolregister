afc2b0e4dec103fdd36cc78b83d9618d33990ada
Krystian.Nowak@gmail.com <Krystian.Nowak@gmail.com@3d4250a1-ed31-0410-8926-6bc40706e74d>
2010-03-27 18:44:43 +0000

    testing export in XML

--- schoolregister/src/test/java/org/schoolregister/importer/StudentImportTest.java
+++ schoolregister/src/test/java/org/schoolregister/importer/StudentImportTest.java
@@ -3,6 +3,8 @@ package org.schoolregister.importer;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
@@ -42,9 +44,26 @@ public class StudentImportTest {
 
 		InputStream is = getClass().getResourceAsStream("input.csv");
 
-		File outputDir = new File(workDir, "output");
-		new StudentImport().doImport(is, outputDir);
+		File actualOutputDir = new File(workDir, "output");
+		new StudentImport().doImport(is, actualOutputDir);
 
+		File expectedOutputDir = new File(workDir, "expected");
+		unpackExpectedOutput(getClass().getResourceAsStream("export.zip"),
+				expectedOutputDir);
+
+		compareXmlDirs(expectedOutputDir, actualOutputDir);
+	}
+
+	private void unpackExpectedOutput(InputStream packedExpectedOutput,
+			File outputDirectory) throws IOException {
+
+		ZipInputStream zis = new ZipInputStream(packedExpectedOutput);
+		ZipEntry entry = zis.getNextEntry();
+		// TODO: unzip file into dir
+	}
+
+	private void compareXmlDirs(File expectedDir, File actualDir) {
+		// TODO: compare xmls inside
 	}
 
 }
