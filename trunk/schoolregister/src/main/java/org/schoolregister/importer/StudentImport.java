3a56e967785cf2f492a246c5532e3cdaa86ce437
Krystian.Nowak@gmail.com <Krystian.Nowak@gmail.com@3d4250a1-ed31-0410-8926-6bc40706e74d>
2010-03-27 17:56:44 +0000

    adding anonymous test data

--- trunk/schoolregister/src/main/java/org/schoolregister/importer/StudentImport.java
+++ trunk/schoolregister/src/main/java/org/schoolregister/importer/StudentImport.java
@@ -1,7 +1,13 @@
 package org.schoolregister.importer;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * 
@@ -9,17 +15,35 @@ import java.util.*;
  * 
  */
 public class StudentImport {
-	public static void main(String[] args) throws Exception {
+
+	private Log log = LogFactory.getLog(getClass());
+
+	public void doImport(InputStream inputData, File outputDir)
+			throws IOException {
 		CsvParser p = new CsvParser();
 
-		List<Student> students = p.parseFile(new FileInputStream(
-				"c:\\_s\\Dane.csv"));
+		List<Student> students = p.parseFile(inputData);
 
 		for (Student student : students) {
-			System.out.println(student.getFirstNames() + " "
+			log.debug("processing student " + student.getFirstNames() + " "
 					+ student.getLastName());
 		}
 
-		XmlExporter.writeXml(students, new File("c:\\_s\\e"));
+		XmlExporter.writeXml(students, outputDir);
+
 	}
+
+	public static void main(String[] args) throws Exception {
+		new StudentImport()
+				.doImport(
+						new FileInputStream(
+								"D:\\devel\\schoolregister\\src\\test\\resources\\org\\schoolregister\\importer\\input.csv"),
+						new File("c:\\_s\\ee"));
+	}
+
+	// public static void main(String[] args) throws Exception {
+	// new StudentImport().doImport(new FileInputStream("c:\\_s\\Dane.csv"),
+	// new File("c:\\_s\\e"));
+	// }
+
 }
